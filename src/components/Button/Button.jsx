8a450cc808fada5e5fea210f557a635aa5c2d51d
eruco <ericlanter@gmail.com>
2022-12-01 18:32:52 -0300

    agregue los eventos

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,18 +2,16 @@ import { useState } from "react";
 import "./button.css";
 
 function Button(props) {
-    
-   const [colorState, setColorState] = useState(
-     { backgroundColor: props.color, 
-       borderColor: "red" }
-     );
-
-   function handleClick() { 
-     setColorState({ backgroundColor: "#cc76cc" });
-   }
+  const [colorState] = useState({
+    backgroundColor: props.color,
+  });
 
   return (
-    <button onClick={handleClick} style={colorState} className="btn">
+    <button
+      onClick={props.onClick}
+      style={colorState}
+      className={`btn ${props.type || ""}`}
+    >
       {props.children}
     </button>
   );
--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -5,29 +5,31 @@ import "./itemcount.css";
 function ItemCount({ stock, onAddToCart, text }) {
   const [count, setCount] = useState(1);
 
-  function handleAdd() {
-    console.log();
+  function handleAdd(evt) {
+    console.log(evt);
     if (count < stock) setCount(count + 1);
   }
 
-  function handleSubstract() {
-    console.log();
+  function handleSubstract(evt) {
+    console.log(evt);
     if (count > 1) setCount(count - 1);
   }
 
   return (
     <div className="itemcount_container">
       <div className="itemcount_control">
-        <Button color="#903024" onClick={handleSubstract}>
+        <Button color="red" onClick={handleSubstract}>
           -
         </Button>
         <span>{count}</span>
-        <Button color="#239044" onClick={handleAdd}>
+        <Button color="blue" onClick={handleAdd}>
           +
         </Button>
       </div>
       <div className="itemcount_btns">
-        <Button onClick={() => onAddToCart(count)}>{text}</Button>
+        <Button type="alert" onClick={() => onAddToCart(count)}>
+          {text}
+        </Button>
       </div>
     </div>
   );
